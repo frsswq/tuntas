a9c26ae49b9d491a8d0c39244f892a8a4582b5e6
frsswq <farissaifuddin.work@gmail.com>
2025-08-08 19:02:11 +0700

    chore: scaffold sveltekit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import favicon from '$lib/assets/favicon.svg';
+  import '../app.css';
+
+  let { children } = $props();
+</script>
+
+<svelte:head>
+  <link rel="icon" href={favicon} />
+</svelte:head>
+
+<nav class="crisp-border sticky top-0 z-10 h-16 bg-white"></nav>
+
+{@render children?.()}
