50e25ba5376b7faa907111c2a21e4ccb2fbaad45
frsswq <farissaifuddin.work@gmail.com>
2025-08-15 14:01:43 +0700

    fix: card stacking positioning not center

--- src/lib/components/app.svelte
+++ src/lib/components/app.svelte
@@ -15,13 +15,13 @@
 <main class="text-sm leading-[1.333] tracking-tight">
   <!-- @TODO: Fix mobile absolute positioning -->
   <section
-    class="relative flex min-h-dvh w-full items-center justify-center overflow-x-scroll bg-slate-50 p-8"
+    class="flex min-h-dvh w-full min-w-dvw items-center justify-center overflow-x-scroll bg-slate-50 p-8"
   >
     {#each positions as pos, index}
       <TodoMain
         todoTitle={title[2 - index]}
         color={color[2 - index]}
-        x={-pos * 20}
+        x={(1 - pos) * 20}
         y={(pos - 1) * 20}
         z={pos}
         containerClass={bg[2 - index]}
@@ -41,5 +41,8 @@
   section {
     background-color: #f8fafc;
     background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%2364748b' fill-opacity='0.1' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E");
+    background-repeat: repeat;
+    background-size: auto;
+    transform: translateZ(0);
   }
 </style>
