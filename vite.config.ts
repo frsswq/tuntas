57b8f72a5e1c630f03ed3e8493d4e2a62d3f8c7d
frsswq <farissaifuddin.work@gmail.com>
2025-08-26 18:51:01 +0700

    wip: drizzle + supabase thingy

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-	server: {
-		port: 3000,
-		strictPort: true
-	},
-	optimizeDeps: {
-		include: ['bits-ui']
-	}
+  plugins: [tailwindcss(), sveltekit()],
+  server: {
+    port: 3000,
+    strictPort: true
+  },
+  optimizeDeps: {
+    include: ['bits-ui']
+  }
 });
