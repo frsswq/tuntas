5c5b0ac5dcf7cd439f4b93921167fe9cd2449fb9
frsswq <farissaifuddin.work@gmail.com>
2025-08-26 17:47:01 +0700

    setup: supabase thingy

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,20 @@
-// See https://svelte.dev/docs/kit/types#app.d.ts
+import { Session, SupabaseClient } from '@supabase/supabase-js';
+// See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
+  namespace App {
+    // interface Error {}
+    interface Locals {
+      supabase: SupabaseClient;
+      safeGetSession(): Promise<{ session: Session | null; user?: Session['user'] | null }>;
+    }
+    interface PageData {
+      session: Session | null;
+      user?: Session['user'] | null;
+    }
+    // interface PageState {}
+    // interface Platform {}
+  }
 }
 
 export {};
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,21 @@
   import '@fontsource-variable/inter';
   import '../app.css';
 
-  let { children } = $props();
+  import { invalidate } from '$app/navigation';
+  import { onMount } from 'svelte';
+
+  let { data, children } = $props();
+  let { supabase, session } = $derived(data);
+
+  onMount(() => {
+    const { data } = supabase.auth.onAuthStateChange((event, _session) => {
+      if (_session?.expires_at !== session?.expires_at) {
+        invalidate('supabase:auth');
+      }
+    });
+
+    return () => data.subscription.unsubscribe();
+  });
 </script>
 
 <svelte:head>
