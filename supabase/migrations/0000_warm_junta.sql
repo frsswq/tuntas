a354bd23d4f4d091951d9897278b1147bc45d98e
frsswq <farissaifuddin.work@gmail.com>
2025-08-27 17:21:19 +0700

    chore: update packages & remove supabase

--- supabase/migrations/0000_warm_junta.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "profile" (
-	"id" uuid PRIMARY KEY NOT NULL,
-	"first_name" text NOT NULL,
-	"last_name" text NOT NULL,
-	"email" text NOT NULL
-);
-
-ALTER LEVEL "profile" ENABLE ROW LEVEL SECURITY;
