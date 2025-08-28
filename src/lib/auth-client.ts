f7cce1f384558e542eb00ddf50b2486016f8050d
frsswq <farissaifuddin.work@gmail.com>
2025-08-28 15:04:04 +0700

    fix: wrong client auth on prod

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,8 @@
+import { PUBLIC_BETTER_AUTH_URL } from '$env/static/public';
 import { createAuthClient } from 'better-auth/svelte';
 
 export const authClient = createAuthClient({
-  baseURL: 'http://localhost:3000'
+  baseURL: PUBLIC_BETTER_AUTH_URL
 });
 
 export const signIn = async (provider: 'google' | 'github') => {
